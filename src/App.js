4342894aa00e33e0b21070e247862f6d7d224191
Jane Philipps <jane.philipps@gmail.com>
2018-03-17 22:12:08 -0700

    initial app setup, create Pixel component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Pixel from './components/Pixel';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Pixel />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Pixel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Pixel extends Component {
+  render() {
+    return (
+      <div className="Pixel">
+        This is a pixel!
+      </div>
+    );
+  }
+}
+
+export default Pixel;
