4d96d0538b948651390b277a2bb50523f0767f35
Jane Philipps <jane.philipps@gmail.com>
2018-03-18 15:16:03 -0700

    make number and size dynamic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
 import Tile from './components/Tile';
+import Input from './components/Input';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+      size: 0
+    }
+  }
+
+
+  _setNumber = function(number) {
+    this.setState({number: number});
+  };
+
+  _setSize = function(size) {
+    this.setState({size: size});
+  };
+
   render() {
+
+    let {
+      number,
+      size
+    } = this.state;
+
     return (
       <div className="App">
+        <Input
+          placeholder="number"
+          onChange={this._setNumber.bind(this)}
+        />
+        <Input
+          placeholder="size"
+          onChange={this._setSize.bind(this)}
+        />
         <Tile
-          number={300}
-          size={4}
+          number={number}
+          size={size}
         />
       </div>
     );
---
+++ src/components/Input.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Input extends Component {
+
+  static propTypes = {
+    placeholder: PropTypes.string,
+    onChange: PropTypes.func
+  }
+
+  _handleChange = function(e) {
+    this.props.onChange(e.target.value);
+  }
+
+  render() {
+
+    let {
+      placeholder
+    } = this.props;
+
+    return (
+      <div className="Input">
+        <input type="number" placeholder={placeholder} onChange={this._handleChange.bind(this)}></input>
+      </div>
+    );
+  }
+}
+
+export default Input;
